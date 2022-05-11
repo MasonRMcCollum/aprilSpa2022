b35341dd5be312c71e88d3f88d78cd8bfddcee50
Mason McCollum <mccollummason34@gmail.com>
2022-05-10 19:09:21 -0500

    5-10-22

---
+++ components/Nav.js
@@ -0,0 +1,10 @@
+import html from "html-literal";
+export default () => `
+<nav>
+<i class="fas fa-bars"></i>
+<ul class="hidden--mobile nav-links">
+  <li><a href="index.html">Home</a></li>
+  <li><a href="bio.html">Bio</a></li>
+  <li><a href="form.html">Form</a></li>
+</nav>
+`;
